016f32a3453ea056b5b1b504162f7449da2c78f0
Chobbi <victor.perezml@uanl.edu.mx>
2021-10-21 18:51:32 -0500

    Segundo avance completo

--- app/src/main/java/com/example/poi/ActivityChatGrupal.kt
+++ app/src/main/java/com/example/poi/ActivityChatGrupal.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.poi.adaptadores.ChatAdaptador
 import com.example.poi.modelos.Mensaje
+import com.example.poi.modelos.Usuario
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
@@ -15,7 +16,9 @@ class ActivityChatGrupal : AppCompatActivity() {
     private val messageList = mutableListOf<Mensaje>()
     private lateinit var chatAdapter: ChatAdaptador
     private lateinit var rvChats: RecyclerView
-    private lateinit var username: String
+    private lateinit var licNombre: String
+    private var username: String = ""
+    private lateinit var licId: String
     private val database = FirebaseDatabase.getInstance()
     private val authen = FirebaseAuth.getInstance()
 
@@ -28,13 +31,17 @@ class ActivityChatGrupal : AppCompatActivity() {
         chatAdapter = ChatAdaptador(messageList)
         rvChats.adapter = chatAdapter
 
-        username = intent.getStringExtra("username") ?: "Sin nombre"
+        licNombre = intent.getStringExtra("licenciaturaChat") ?: "Sin nombre"
+        licId = intent.getStringExtra("keyChat") ?: "Sin nombre"
+
+        val fromId = authen.uid ?: ""
+        getUsername(fromId)
 
         findViewById<Button>(R.id.btn_Enviar).setOnClickListener {
             val message = findViewById<EditText>(R.id.edit_EnviarMsg).text.toString()
             if (message.isNotEmpty()) {
                 val fromId = authen.uid ?: ""
-                val toId = "WDVqLyeGw9Vn290xeHcj5PkWTnb2"
+                val toId = licId
                 val msg = Mensaje("", message, username, ServerValue.TIMESTAMP, fromId, toId)
                 sendMessage(msg)
             }
@@ -45,7 +52,7 @@ class ActivityChatGrupal : AppCompatActivity() {
     }
 
     private fun sendMessage(message: Mensaje) {
-        val reference = database.getReference("/chat-group/lmad").push()
+        val reference = database.getReference("/chat-group/$licId").push()
 
         message.id = reference.key ?: ""
 
@@ -54,8 +61,8 @@ class ActivityChatGrupal : AppCompatActivity() {
 
     private fun readMessage() {
         val fromId = authen.uid ?: ""
-        val toId = "WDVqLyeGw9Vn290xeHcj5PkWTnb2"
-        database.getReference("/chat-group/lmad").addValueEventListener(object :
+        val toId = licId
+        database.getReference("/chat-group/$toId").addValueEventListener(object :
             ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 messageList.clear()
@@ -69,6 +76,20 @@ class ActivityChatGrupal : AppCompatActivity() {
                     chatAdapter.notifyDataSetChanged()
                     rvChats.smoothScrollToPosition(messageList.size - 1)
                 }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+    public fun getUsername(id : String){
+        val ref = database.getReference("/usuarios/$id")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val currentUser: Usuario = snapshot.getValue(Usuario::class.java) as Usuario
+                username = currentUser.nombreUsuario
             }
 
             override fun onCancelled(error: DatabaseError) {
